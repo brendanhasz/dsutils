71bdb5a8c283e9e8e51d127bec2ee49a6b7a2d79
Brendan Hasz <winsto99@gmail.com>
2019-02-21 16:32:20 -0600

    Add quantile transform and mutual information

---
+++ src/dsutils/metrics.py
@@ -0,0 +1,43 @@
+"""Metrics
+"""
+
+import numpy as np
+
+from .transforms import quantile_transform
+
+
+def mutual_information(xi, yi, res=20):
+    """Compute the mutual information between two vectors
+
+    Parameters
+    ----------
+    xi : ndarray or series
+    yi : ndarray or series
+
+    """
+    ix = ~(np.isnan(xi) | np.isinf(xi) | np.isnan(yi) | np.isinf(yi))
+    x = xi[ix]
+    y = yi[ix]
+    N, xe, ye = np.histogram2d(x, y, res)
+    Nx, _ = np.histogram(x, xe)
+    Ny, _ = np.histogram(y, ye)
+    N = N / len(x) #normalize
+    Nx = Nx / len(x)
+    Ny = Ny / len(y)
+    Ni = np.outer(Nx, Ny)
+    Ni[Ni == 0] = np.nan
+    N[N == 0] = np.nan
+    return np.nansum(N * np.log(N / Ni))
+    
+
+def q_mut_info(x, y):
+    """Compute the mutual information between two quantile-transformed vectors
+
+    Parameters
+    ----------
+    x : ndarray or series
+    y : ndarray or series
+
+    """
+    return mutual_information(quantile_transform(x),
+                              quantile_transform(y))
---
+++ src/dsutils/transforms.py
@@ -0,0 +1,13 @@
+"""Transforms
+
+"""
+
+
+import numpy as np
+
+
+def quantile_transform(v, res=101):
+    """Quantile-transform a vector to lie between 0 and 1"""
+    x = np.linspace(0, 100, res)
+    prcs = np.nanpercentile(v, x)
+    return np.interp(v, prcs, x/100.0)
