c8a85a1ee67d1008c61494e72d4d55142620d836
Brendan Hasz <haszx010@umn.edu>
2019-02-21 15:32:30 -0600

    Add function to convert categorical columns to integers

--- src/dsutils/cleaning.py
+++ src/dsutils/cleaning.py
@@ -37,3 +37,51 @@ def remove_noninformative_cols(df):
         elif df[col].nunique()<2:
             print('Removing '+col+' (<2 unique values)')
             del df[col]
+
+
+def categorical_to_int(df, cols=None):
+    """Convert categorical columns of a DataFrame to unique integers, inplace.
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        DataFrame in which to convert columns
+    cols : list
+    """
+
+    # Do for all "object" columns if not specified
+    if cols is None:
+        cols = [col for col in df if str(df[col].dtype)=='object']
+    if len(cols) == 0:
+        return
+
+    # Make list if not
+    if isinstance(cols, str):
+        cols = [cols]
+
+    # Map each column
+    maps = dict()
+    for col in cols:
+
+        # Create the map from objects to integers
+        maps[col] = dict(zip(
+            df[col].values, 
+            df[col].astype('category').cat.codes.values
+        ))
+
+        # Determine appropriate datatype
+        max_val = max(maps[col].values())
+        if max_val < 256:
+            dtype = 'uint8'
+        elif max_val < 65536:
+            dtype = 'uint16'
+        elif max_val < 4294967296:
+            dtype = 'uint32'
+        else:
+            dtype = 'uint64'
+
+        # Map the column
+        df[col] = df[col].map(maps[col]).astype(dtype)
+
+    # Return the maps used
+    return maps
