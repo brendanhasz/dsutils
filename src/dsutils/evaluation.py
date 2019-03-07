782c3fd8b10364a82bfa508170c049d828bae599
Brendan Hasz <winsto99@gmail.com>
2019-03-07 01:01:15 -0600

    Add cv hyperparameter optimization, still need to implement gaussian process for next param choice

--- src/dsutils/evaluation.py
+++ src/dsutils/evaluation.py
@@ -5,6 +5,8 @@
 
 """
 
+import time
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,8 +14,14 @@ import seaborn as sns
 from sklearn.base import BaseEstimator
 from sklearn.base import clone
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
-from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.metrics import accuracy_score, roc_auc_score, make_scorer
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
+
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RationalQuadratic
+
 
 
 
@@ -177,6 +185,201 @@ def plot_permutation_importance(importances):
     sns.barplot(x='Importance', y='Feature', data=df, order=dfg['Feature'])
 
 
-# TODO: binary_feature_search, which does a binary search using permutation
-# based feature importances to find the set of features which give the best
-# results (as determined by the passed metric)
+
+def optimize_cv(X, y, model, bounds,
+                n_splits=3,
+                max_time=None,
+                max_evals=20,
+                optimizer='bayesian',
+                n_jobs=1,
+                metric='mse'):
+    """Optimize model parameters using cross-fold validation.
+
+    Parameters
+    ----------
+    X : pandas DataFrame
+        Independent variable values (features)
+    y : pandas Series
+        Dependent variable values (target)
+    model : sklearn Pipeline
+        Predictive model to optimize
+    bounds : dict
+        Parameter bounds.  A dict where the keys are the parameter names, and
+        the values are the parameter bounds and type (as a tuple).  Each
+        parameter name should be the Pipeline step string, then a double
+        underscore, then the parameter name (see example below). Each 
+        tuple should be (lower_bound, upper_bound, type), where type is int or
+        float.
+    n_splits : int
+        Number of cross-validation folds.
+    max_time : None or float
+        Give up after this many seconds
+    max_evals : int
+        Max number of cross-validation evaluations to perform.
+    optimizer : str
+        Which method to use for optimization.  'random' to make random 
+        parameter choices and take the best.  'bayesian' to use Gaussian
+        process optimization.
+    n_jobs : int
+        Number of parallel jobs to run (for cross-validation).    
+    metric : str or sklearn scorer
+        What metric to use for evaluation.  One of:
+
+        * 'r2' - coefficient of determination (maximize)
+        * 'mse' - mean squared error (minimize)
+        * 'mae' - mean absolute error (minimize)
+        * 'accuracy' or 'acc' - accuracy (maximize)
+        * 'auc' - area under the ROC curve (maximize)
+
+    Returns
+    -------
+    opt_params : dict
+        Optimal parameters.  Dict of the same format as bounds, except instead
+        of tuples, the values contain the optimal parameter values.
+    params : dict
+        All the parameters which were evaluated.  Dict just like opt_params,
+        except the values are vectors of all parameters which were evaluated.
+    scores : ndarray
+        Vector of scores for each evaluation.  Same size as each value in
+        params
+
+    Example
+    -------
+
+    from sklearn.pipeline import Pipeline
+    from sklearn.decomposition import PCA
+    from sklearn.linear_model import Ridge
+
+    model = Pipeline([
+        ('pca', PCA(n_components=5)),
+        ('regressor', Ridge(alpha=1.0))
+    ])
+
+    bounds = {
+        'pca__n_components': [1, 100, int],
+        'regressor__alpha': [0, 10, float]
+    }
+
+    opt_params, _, _ = optimize_cv(X, y, model, bounds)
+
+    opt_params['pca']['n_components'] #optimal # components
+    opt_params['regressor']['alpha'] #optimal alpha value
+    """
+
+    # Check inputs
+    if not isinstance(X, pd.DataFrame):
+        raise TypeError('X must be a pandas DataFrame')
+    if not isinstance(y, (pd.Series, np.ndarray)):
+        raise TypeError('y must be a pandas Series')
+    if X.shape[0] != y.shape[0]:
+        raise ValueError('X and y must have the same number of samples')
+    if not isinstance(model, Pipeline):
+        raise TypeError('model must be an sklearn Pipeline')
+    if not isinstance(bounds, dict):
+        raise TypeError('bounds must be a dict')
+    if not isinstance(n_splits, int):
+        raise TypeError('n_splits must be an integer')
+    if n_splits < 1:
+        raise ValueError('n_splits must be one or greater')
+    if max_time is not None and not isinstance(max_time, float):
+        raise TypeError('max_time must be None or a float')
+    if max_time is not None and max_time < 0:
+        raise ValueError('max_time must be positive')
+    if not isinstance(max_evals, int):
+        raise TypeError('max_evals must be an int')
+    if max_evals < 1:
+        raise ValueError('max_evals must be positive')
+    if not isinstance(optimizer, str):
+        raise TypeError('optimizer must be a string')
+    if optimizer not in ['random', 'bayesian']:
+        raise ValueError('optimizer must be \'random\' or \'bayesian\'')
+    if not isinstance(n_jobs, int):
+        raise TypeError('n_jobs must be an int')
+    if n_jobs < 1:
+        raise ValueError('n_jobs must be positive')
+
+    # Create scorer
+    if metric == 'r2':
+        scorer = make_scorer(r2_score)
+    elif metric == 'mse':
+        scorer = make_scorer(mean_squared_error)
+    elif metric == 'mae':
+        scorer = make_scorer(mean_absolute_error)
+    elif metric == 'accuracy' or metric == 'acc':
+        scorer = make_scorer(accuracy_score)
+    elif metric == 'auc':
+        scorer = make_scorer(roc_auc_score)
+    elif hasattr(metric, '__call__'):
+        scorer = metric
+    else:
+        raise ValueError('metric must be a metric string or a callable')
+
+    # Flip the score depending on the metric, such that lower is better
+    if metric == 'mse' or metric == 'mae':
+        flip = 1
+    else:
+        flip = -1
+
+    # Create Gaussian process model
+    if optimizer == 'bayesian':
+        gp = GaussianProcessRegressor(kernel=RationalQuadratic(),
+                                      n_restarts_optimizer=5,
+                                      normalize_y=True)
+
+    # Initialize arrays to store evaluated parameters
+    scores = []
+    params = dict()
+    new_params = dict()
+    for param in bounds:
+        params[param] = []
+
+    # Search for optimal parameters
+    start_time = time.time()
+    for i in range(max_evals):
+
+        # Give up if we've spent too much time
+        if max_time is not None and time.time()-start_time > max_time:
+            break
+
+        # Randomly choose next parameter values to try
+        if optimizer == 'random':
+            for param, bound in bounds.items():
+                new_params[param] = np.random.uniform(bound[0], bound[1])
+
+        # Bayesian optimizer
+        else:
+            pass
+            # TODO
+
+        # Convert integer params to integer
+        for param, bound in bounds.items():
+            if bound[2] is int:
+                new_params[param] = round(new_params[param])
+
+        # Modify model to use new parameters, and store values
+        for param_name, val in new_params.items():
+            step = param_name.split('__')[0]
+            param = param_name.split('__')[1]
+            model.named_steps[step].set_params(**{param: val})
+            params[param_name].append(val)
+
+        # Compute and store cross-validated metric
+        t_score = cross_val_score(model, X, y, cv=n_splits, 
+                                  scoring=scorer, n_jobs=n_jobs)
+        scores.append(t_score.mean())
+
+    # Get best evaluated parameters if using random optimization
+    if optimizer == 'random':
+        opt_params = dict()
+        ix = scores.index(min(flip*e for e in scores)) #index of best score
+        for param, vals in params.items():
+            opt_params[param] = vals[ix]
+
+    # Use Gaussian process to find max value if using Bayesian optimization
+    else:
+        pass
+        # TODO
+
+    # Return optimal parameters, all evaluated parameters, and scores
+    return opt_params, params, scores
+
--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -2,6 +2,8 @@
 
 """
 
+import time
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,15 +12,19 @@ from sklearn.datasets import make_regression, make_classification
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.linear_model import Ridge, ElasticNet
 from sklearn.metrics import median_absolute_error, jaccard_similarity_score
+from sklearn.decomposition import PCA
 
 from dsutils.evaluation import permutation_importance
 from dsutils.evaluation import permutation_importance_cv
 from dsutils.evaluation import plot_permutation_importance
+from dsutils.evaluation import optimize_cv
+
 
 
 def test_permutation_importance_regression(plot):
-    """Tests encoding.permutation_importance w/ regression problems"""
+    """Tests evaluation.permutation_importance w/ regression problems"""
 
     # Make dummy regression dataset
     N = 1000
@@ -64,7 +70,7 @@ def test_permutation_importance_regression(plot):
 
 
 def test_permutation_importance_classification():
-    """Tests encoding.permutation_importance w/ classification problems"""
+    """Tests evaluation.permutation_importance w/ classification problems"""
 
     # Make dummy classification dataset
     N = 1000
@@ -103,7 +109,7 @@ def test_permutation_importance_classification():
 
 
 def test_permutation_importance_cv():
-    """Tests encoding.permutation_importance_cv"""
+    """Tests evaluation.permutation_importance_cv"""
 
     # Make dummy regression dataset
     N = 1000
@@ -129,7 +135,7 @@ def test_permutation_importance_cv():
 
 
 def test_plot_permutation_importance(plot):
-    """Tests encoding.plot_permutation_importance"""
+    """Tests evaluation.plot_permutation_importance"""
 
     # Make dummy regression dataset
     N = 1000
@@ -154,4 +160,170 @@ def test_plot_permutation_importance(plot):
 
     plot_permutation_importance(imp_df)
     if plot:
+        plt.show()
+
+
+
+def test_optimize_cv_random_1(plot):
+    """Tests evaluation.optimize_cv w/ regression problems"""
+
+    # Make dummy regression dataset
+    N = 1000
+    X, y = make_regression(n_samples=N,
+                           n_features=5,
+                           n_informative=2,
+                           n_targets=1,
+                           random_state=12345,
+                           shuffle=False,
+                           noise=1.0)
+    df = pd.DataFrame(X, columns=['a', 'b', 'c', 'd', 'e'])
+    df['y'] = y
+    X_train = df.loc[:N/2-1, ['a', 'b', 'c', 'd', 'e']].copy()
+    y_train = df.loc[:N/2-1, 'y'].copy()
+    X_test = df.loc[N/2:, ['a', 'b', 'c', 'd', 'e']].copy()
+    y_test = df.loc[N/2:, 'y'].copy()
+
+    # Create a regression pipeline
+    model = Pipeline([
+        ('regressor', Ridge(alpha=1.0)),
+    ])
+
+    # Define bounds
+    bounds = {
+        'regressor__alpha': [0.0, 2.0, float]
+    }
+
+    # Find best parameters
+    opt_params, params, scores = optimize_cv(X_train, y_train, model, bounds,
+                                             n_splits=3,
+                                             max_time=None,
+                                             max_evals=10,
+                                             optimizer='random',
+                                             metric='mse')
+
+    # Plot parameters and their scores
+    if plot:
+        plt.plot(params['regressor__alpha'], scores, '.')
+        plt.xlabel('regressor__alpha')
+        plt.ylabel('mse')
+        plt.show()
+
+    # Check n_jobs arg works
+    opt_params, params, scores = optimize_cv(X_train, y_train, model, bounds,
+                                             n_splits=3,
+                                             max_time=None,
+                                             max_evals=10,
+                                             optimizer='random',
+                                             metric='mse',
+                                             n_jobs=2)
+
+    # Check timeout works
+    start_time = time.time()
+    opt_params, params, scores = optimize_cv(X_train, y_train, model, bounds,
+                                             n_splits=3,
+                                             max_time=0.2,
+                                             max_evals=int(1e12),
+                                             optimizer='random',
+                                             metric='mse')
+    assert time.time()-start_time < 1.0
+
+
+
+
+
+def test_optimize_cv_random_2(plot):
+    """Tests evaluation.optimize_cv w/ 2 parameters"""
+
+    # Make dummy regression dataset
+    N = 1000
+    X, y = make_regression(n_samples=N,
+                           n_features=5,
+                           n_informative=2,
+                           n_targets=1,
+                           random_state=12345,
+                           shuffle=False,
+                           noise=1.0)
+    df = pd.DataFrame(X, columns=['a', 'b', 'c', 'd', 'e'])
+    df['y'] = y
+    X_train = df.loc[:N/2-1, ['a', 'b', 'c', 'd', 'e']].copy()
+    y_train = df.loc[:N/2-1, 'y'].copy()
+    X_test = df.loc[N/2:, ['a', 'b', 'c', 'd', 'e']].copy()
+    y_test = df.loc[N/2:, 'y'].copy()
+
+    # Create a regression pipeline
+    model = Pipeline([
+        ('regressor', ElasticNet(alpha=1.0, l1_ratio=0.5)),
+    ])
+
+    # Define bounds
+    bounds = {
+        'regressor__alpha': [1e-4, 2.0, float],
+        'regressor__l1_ratio': [0.0, 1.0, float]
+    }
+
+    # Find best parameters
+    opt_params, params, scores = optimize_cv(X_train, y_train, model, bounds,
+                                             n_splits=3,
+                                             max_time=None,
+                                             max_evals=10,
+                                             optimizer='random',
+                                             metric='mse')
+
+    # Plot parameters and their scores
+    if plot:
+        plt.scatter(params['regressor__alpha'],
+                    params['regressor__l1_ratio'],
+                    c=scores, alpha=0.7)
+        plt.xlabel('regressor__alpha')
+        plt.ylabel('regressor__l1_ratio')
+        plt.show()
+
+
+
+def test_optimize_cv_random_int(plot):
+    """Tests evaluation.optimize_cv w/ int parameter types"""
+
+    # Make dummy regression dataset
+    N = 1000
+    X, y = make_regression(n_samples=N,
+                           n_features=5,
+                           n_informative=2,
+                           n_targets=1,
+                           random_state=12345,
+                           shuffle=False,
+                           noise=1.0)
+    df = pd.DataFrame(X, columns=['a', 'b', 'c', 'd', 'e'])
+    df['y'] = y
+    X_train = df.loc[:N/2-1, ['a', 'b', 'c', 'd', 'e']].copy()
+    y_train = df.loc[:N/2-1, 'y'].copy()
+    X_test = df.loc[N/2:, ['a', 'b', 'c', 'd', 'e']].copy()
+    y_test = df.loc[N/2:, 'y'].copy()
+
+    # Create a regression pipeline
+    model = Pipeline([
+        ('pca', PCA(n_components=5)),
+        ('regressor', Ridge(alpha=1.0)),
+    ])
+
+    # Define bounds
+    bounds = {
+        'pca__n_components': [1, 5, int],
+        'regressor__alpha': [0.0, 4.0, float]
+    }
+
+    # Find best parameters
+    opt_params, params, scores = optimize_cv(X_train, y_train, model, bounds,
+                                             n_splits=3,
+                                             max_time=None,
+                                             max_evals=10,
+                                             optimizer='random',
+                                             metric='mse')
+
+    # Plot parameters and their scores
+    if plot:
+        plt.scatter(params['pca__n_components'],
+                    params['regressor__alpha'],
+                    c=scores, alpha=0.7)
+        plt.xlabel('pca__n_components')
+        plt.ylabel('regressor__alpha')
         plt.show()
