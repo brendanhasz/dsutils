12c8b5083c880804052d22197b18e6de199b8af0
Brendan Hasz <winsto99@gmail.com>
2019-04-02 22:20:18 -0500

    Add SvdRegressor, which uses the suprise library to do user-item reccomendation

--- src/dsutils/models.py
+++ src/dsutils/models.py
@@ -1,8 +1,7 @@
 """Models
 
-* :func:`.permutation_importance`
-* :func:`.permutation_importance_cv`
-* :func:`.plot_permutation_importance`
+* :class:`.InterpolatingPredictor`
+* :class:`.SvdRegressor`
 
 """
 
@@ -11,8 +10,9 @@ import pandas as pd
 from scipy.optimize import minimize
 
 from sklearn.metrics import mean_squared_error
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, RegressorMixin
 
+import surprise
 
 
 class InterpolatingPredictor(BaseEstimator):
@@ -74,6 +74,127 @@ class InterpolatingPredictor(BaseEstimator):
         return (self._weights.reshape(1, -1)*X).sum(axis=1)
 
 
+    def fit_predict(self, X, y):
+        """
+        TOOD
+        """
+        return self.fit(X, y).predict(X)
+
+
+
+class SvdRegressor(BaseEstimator, RegressorMixin):
+    """Uses suprise's SVD to predict user scores of items
+    
+    TODO: docs
+    
+    Parameters
+    ----------
+    user_col : int or str
+        Column of X to use as user IDs.
+        Default is 0.
+    item_col : int or str
+        Column of X to use as item IDs.
+        Default is 1.
+    score_range : tuple of int or float
+        Range of the scores in y.  First element should be lower 
+        bound and second element should be upper bound.
+        Default is to use the minimum and maximum y values.
+    n_factors : int
+        Number of factors to use (dimensionality of the user and item embeddings).
+        Default = 100
+    n_epochs : int
+        Number of epochs to train for.
+        Default = 20
+    **kwargs
+        Additional kwargs are passed to 
+        surprise.prediction_algorithms.matrix_factorization.SVD.
+    
+    """
+    
+    def __init__(self, user_col=0, item_col=1, score_range=(None, None), **kwargs):
+        """Create the regressor"""
+        
+        # Check inputs
+        if not isinstance(user_col, (int, str)):
+            raise TypeError('user_col must be a str or an int')
+        if not isinstance(item_col, (int, str)):
+            raise TypeError('item_col must be a str or an int')
+        if not isinstance(score_range, tuple):
+            raise TypeError('score_range must be a tuple')
+        if len(score_range) != 2:
+            raise ValueError('score_range must be a tuple with 2 elements')
+        if not all(isinstance(e, (int, float)) for e in score_range):
+            raise TypeError('score_range must contain ints or floats')
+        if score_range[0] > score_range[1]:
+            raise ValueError('First element of score_range must be less than second element')
+            
+        # Store parameters
+        self._user_col = user_col
+        self._item_col = item_col
+        self._score_range = score_range
+        self._kwargs = kwargs
+        self._model = None
+
+
+    def fit(self, X, y):
+        """Fit the SVD model to data
+        
+        Parameters
+        ----------
+        X : pandas DataFrame
+            Table containing user IDs and item IDs.
+        y : pandas Series
+            Scores.
+        """
+
+        # Create a pandas DataFrame in suprise format
+        X_train = X[[self._user_col, self._item_col]]
+        X_train['score'] = y
+        
+        # Compute the score range
+        if self._score_range[0] is None:
+            self._score_range[0] = y.min()
+        if self._score_range[1] is None:
+            self._score_range[1] = y.max()
+        
+        # Create a suprise Dataset from the dataframe
+        reader = surprise.Reader(rating_scale=self._score_range)
+        dataset = (surprise.dataset.Dataset
+                   .load_from_df(X_train, reader))
+        dataset = dataset.build_full_trainset()
+
+        # Fit the model
+        self._model = surprise.SVD(**self._kwargs)
+        self._model.fit(dataset)
+        return self
+    
+
+    def predict(self, X, y=None):
+        """
+        TODO
+        """
+
+        # Check if model has been fit
+        if self._model is None:
+            raise RuntimeError('model has not been fit')
+        
+        # Create a pandas DataFrame in suprise format
+        X_test = X[[self._user_col, self._item_col]]
+        X_test['score'] = np.nan
+        
+        # Create a suprise Testset from the dataframe
+        reader = surprise.Reader(rating_scale=self._score_range)
+        testset = (surprise.dataset.Dataset
+                   .load_from_df(X_test, reader))
+        testset = testset.build_full_trainset().build_testset()
+        
+        # Use suprise model to predict scores
+        preds = self._model.test(testset) #returns a list of "Prediction" objs...
+        preds = [pred[3] for pred in preds]
+        return pd.Series(data=np.array(preds),
+                         index=X.index)
+        
+        
     def fit_predict(self, X, y):
         """
         TOOD
